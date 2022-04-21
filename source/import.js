e26e1f6370445e2e538e36908b0b315fa46bbfd3
coderzhao <coderzhaoziwei@outlook.com>
2022-04-21 17:47:12 +0800

    feat: add eslint-plugin-import

---
+++ source/import.js
@@ -0,0 +1,12 @@
+// eslint-plugin-import
+// https://github.com/import-js/eslint-plugin-import
+
+module.exports = {
+  plugins: [
+    'import',
+  ],
+  extends: [
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+}
