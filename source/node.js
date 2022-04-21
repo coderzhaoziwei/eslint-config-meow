1ab4e4a8e84ca86e7ff8f04b17df28361fb87bf8
coderzhao <coderzhaoziwei@outlook.com>
2022-04-21 17:47:36 +0800

    chore: add eslint-plugin-node

---
+++ source/node.js
@@ -0,0 +1,8 @@
+// eslint-plugin-node
+// https://github.com/mysticatea/eslint-plugin-node
+
+module.export = {
+  parserOptions: { ecmaVersion: 2020 },
+  plugins: ['node'],
+  extends: ['plugin:node/recommended'],
+}
